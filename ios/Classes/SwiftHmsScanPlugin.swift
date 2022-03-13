cd901902e7d1f91db1e17435e393268009741e78
leiguangwu <guangwu.lei@genlot.com>
2022-03-13 18:26:27 +0800

    新增ios插件

--- ios/Classes/SwiftHmsScanPlugin.swift
+++ ios/Classes/SwiftHmsScanPlugin.swift
@@ -1,14 +1,68 @@
 import Flutter
 import UIKit
+import ScanKitFrameWork
 
-public class SwiftHmsScanPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
+public class SwiftHmsScanPlugin: NSObject, FlutterPlugin, DefaultScanDelegate {
+    
+    private var result: FlutterResult?
+    private var hostViewController: UIViewController?
+    
+    public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "hms_scan", binaryMessenger: registrar.messenger())
     let instance = SwiftHmsScanPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+      self.result = result
+      if ("loadScanKit" == call.method) {
+          loadScanKit()
+      } else {
+          result("iOS " + UIDevice.current.systemVersion)
+      }
   }
+    
+    public func loadScanKit() {
+        
+        if let rootVC = UIApplication.shared.keyWindow?.rootViewController {
+                    hostViewController = topViewController(base:rootVC)
+                } else if let window = UIApplication.shared.delegate?.window,let rootVC = window?.rootViewController {
+                    hostViewController = topViewController(base:rootVC)
+                }
+        
+        let hmsDefaultScanViewController = HmsDefaultScanViewController()
+
+        let navigationController = UINavigationController(rootViewController: hmsDefaultScanViewController)
+        
+        if #available(iOS 13.0, *) {
+              navigationController.modalPresentationStyle = .fullScreen
+          }
+          
+        hmsDefaultScanViewController.defaultScanDelegate = self
+        hostViewController?.present(navigationController, animated: false)
+    }
+    
+    private func topViewController(base: UIViewController?) -> UIViewController? {
+        if let nav = base as? UINavigationController {
+            return topViewController(base: nav.visibleViewController)
+
+        } else if let tab = base as? UITabBarController, let selected = tab.selectedViewController {
+            return topViewController(base: selected)
+
+        } else if let presented = base?.presentedViewController {
+            return topViewController(base: presented)
+        }
+        return base
+    }
+    
+    private func defaultScanDelegateForDicResult(resultDic: NSDictionary){
+        let string = resultDic["text"]
+        result?(string)
+    }
+    
+    private func defaultScanImagePickerDelegateForImage(image : UIImage){
+        let resultDic = HmsBitMap.bitMap(for: image, with: HmsScanOptions())
+        let string = resultDic?["text"]
+        result?(string)
+    }
 }
